b72ca56fede0472544f11d62bd0fe761978caf75
Ezra-Crowe <ecrowe@oberlin.edu>
2025-02-22 20:57:51 -0500

    added some changes to gemini prompt and made a dummy .env showing the user where to put the API key

--- backend/GeminiSimplify.py
+++ backend/GeminiSimplify.py
@@ -12,7 +12,7 @@ client = genai.Client(api_key=api_key)
 jargonfile = client.files.upload(file='atc_audio.wav')
 response = client.models.generate_content(
         model='gemini-2.0-flash',
-        contents=['This is audio from Air Traffic Control. Consider the average person at an aiport. They dont want to learn all the jargon to understand the incoming audio signals, and just want someone to do it for them in a short, concise manner. Please give information that is relevant to the average person at the airport and be as informative as possible. List all events that you can parse out, and how it might be relevant to the user. Produce a feed that could show up on an app for those trying to figure out whats going on the airport (imagine an app for this purpose). List each event as something that would be understandable to an average user',
+        contents=['This is audio from Air Traffic Control. Consider the average person at an aiport. They dont want to learn all the jargon to understand the incoming audio signals, and just want someone to do it for them in a short, concise manner. Please give information that is relevant to the average person at the airport and be as informative as possible. List all events that you can parse out, and how it might be relevant to the user. Produce a feed that could show up on an app for those trying to figure out whats going on the airport (imagine an app for this purpose). List each event as something that would be understandable to an average user. Since this is for an app, please start each event with a [event] and end it with a $ so the app can process this data.',
                   jargonfile
                   ]
         )
